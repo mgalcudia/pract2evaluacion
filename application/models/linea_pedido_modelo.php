ef067bc76dcb88fdbca1a1d48cafca7fd0f476b4
mgalcudia <>
2015-02-24 01:14:36 +0100

    añadido categorias a modelo

---
+++ application/models/linea_pedido_modelo.php
@@ -0,0 +1,43 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Lineas_Pedidos_model extends CI_Model {
+    
+    function __construct() {
+        parent::__construct();
+    }
+    
+    /**
+     * Lista las lineas de pedidos
+     * @return type
+     */
+    function listar_linea_pedidos(){
+        $query = $this->db->get('linea_pedido');
+        return $query->result_array();
+    }
+        
+    
+    /**
+     * Crea un nuevo linea_linea_pedido
+     * @param type $datos
+     */
+    function crear_linea_pedido($datos){
+        $this->db->insert('linea_pedido', $datos);
+    }
+    /**
+     * Busca lineas de pedido por $datos
+     * @param type $datos
+     * @return type
+     */
+    function buscar_linea_pedidos($datos){
+        $this->db->where($datos);
+        $query = $this->db->get('linea_pedido');
+        return $query->result_array();
+    }
+
+    
+}
+
+
+
+
+
